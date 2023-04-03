0c09d8e441267a83fb254b3d2d93b9009640e6eb
Aftermath001 <alvinasiachi@gmail.com>
2023-04-03 21:04:07 +0300

    Second Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+    fetch("http://localhost:3000/Doctors")
+    .then (response => response.json())
+    .then (data =>{
+     const Kona = document.querySelector(".services");
+     for(let i=0;i<data.length;i++){
+         let name = data[i].name;
+         let gender = data[i].gender;
+         let phone=data[i].phone;
+         let price = data[i].price;
+         let description =data[i].description;
+         let image= data[i].image;
+         const cards =document.createElement("div");
+         cards.classList.add('cards');
+         cards.innerHTML+=`
+         <ol><p style = "color:white">${name}</p>
+         <p style = "color:white">${gender}</p>
+         <p style = "color:white">${phone}</p>
+         <p style = "color:white">${price}</p>
+         <img src='${image}'style= "height:200px;width:200px" ><ol>
+         `
+         Kona.appendChild(cards);
+         }
+  })
+
