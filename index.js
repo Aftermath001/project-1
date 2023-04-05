f1e79b5be32ea8d3452acb1d44eca5fc970871f5
Aftermath001 <alvinasiachi@gmail.com>
2023-04-05 21:15:18 +0300

    Added Rating

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+
     fetch("http://localhost:3000/Doctors")
     .then (response => response.json())
     .then (data =>{
@@ -10,7 +11,7 @@
          let price = data[i].price;
          let image= data[i].image;
          const cards =document.createElement("div");
-         cards.classList.add('cards');
+         cards.classList.add('cards',"i.category");
          cards.innerHTML+=`
          <ol>
          <img src='${image}'style= "height:200px;width:200px" >
@@ -18,9 +19,59 @@
          <p style = "color:white">${gender}</p>
          <p style = "color:white">${speciality}</p>
          <p style = "color:white">${price}</p>
+         <span class="fa fa-star"></span>
+         <span class="fa fa-star"></span>
+         <span class="fa fa-star"></span>
+         <span class="fa fa-star"></span>
+         <span class="fa fa-star"></span>
          <ol>
          `
          Kona.appendChild(cards);
          }
   })
 
+
+function filterDoctors(value){
+
+     let buttons = document.querySelectorAll(".button-value");
+      
+     buttons.forEach((button) => {
+
+        if(value.toUpperCase() == button.innerText.toUpperCase()){
+
+            button.classList.add("active");
+        }
+        else{
+            button.classList.remove("active");
+        }
+     })
+     let elements = document.querySelectorAll(".cards");
+
+     elements.forEach((elements) => {
+         
+        if (value == "All"){
+            elements.classList.remove("hide")
+        }
+     })
+}
+document.getElementById("Search").addEventListener("click",() => {
+   
+    let searchInput = document.getElementById("Search-input").value;
+    let elements = document.querySelectorAll ("Doctor-name")
+
+     
+
+
+
+
+}
+
+
+
+
+)
+window.onload = () => {
+    filterDoctors ("All")
+}
+
+  
